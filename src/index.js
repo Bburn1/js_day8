39cab0bbe7c798e6e32c9c62aff481e43f0fa3b4
”Bburn1” <bogdanburn123@gmail.com>
2022-02-12 13:48:35 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let one = 10
+let two = 19
+let three = 30
+
+let result = (one < 10) ? "one < 10" : (two >= 20) ? "two >= 20" : (three >= 30) ? "three >= 30" : "three < 30";
+
+
+console.log(result);
+
